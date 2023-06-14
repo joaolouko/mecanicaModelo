c5c01dad86979bfd71f210833b827f7c7c9d308f
joaolouko <joaopiresmachadonunes@gmail.com>
2023-06-13 22:10:47 -0300

    icones incriveis do curso, ameeiiiii

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,6 +1,17 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+
+import styles from './Footer.module.css'
+
 function Footer() {
     return(
-        <footer>Rodape</footer>
+        <footer>
+            <ul className={styles.social_list}>
+                <li><FaFacebook/></li>
+                <li><FaInstagram/></li>
+                <li><FaLinkedin/></li>
+            </ul>
+            <p>Nosso rodape</p>
+        </footer>
     )
 }
 
