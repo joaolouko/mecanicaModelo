50074c347c7a4b590a1b51aade0b499b437895e3
joaolouko <joaopiresmachadonunes@gmail.com>
2023-06-21 19:45:42 -0300

    versão alfa 1.0.2 - pagina de inicio/flex e cards, correção de bugs sobre o celular parcialmente

--- src/components/Acoes.js
+++ src/components/Acoes.js
@@ -1,7 +1,15 @@
+import styles from './Acoes.module.css'
+
 function Acoes() {
-    return(
+    return (
         <div>
-            <p>pirir popo</p>
+            <div className={styles.container}>
+                <div className={styles.card}>
+                    <h1>Coisa realizada</h1>
+                    <h3>Mudança de nomes na oficina</h3>
+                </div>
+            </div>
+
         </div>
     )
 }
--- src/components/pages/Inicio.js
+++ src/components/pages/Inicio.js
@@ -1,12 +1,18 @@
 import Acoes from '../Acoes'
 import styles from './Inicio.module.css'
 
-function Inicio(){
-    return(
+function Inicio() {
+    return (
         <div>
-            <h1 className={styles.titulo}>Bem vindo Cristiano</h1>
-            <p className={styles.texto}>Aqui estão as suas ultimas ações</p>
-            <Acoes/>
+            <div className={styles.container_titulo}>
+                <h1 className={styles.titulo}>Bem vindo Cristiano</h1>
+                <h2 className={styles.texto}>Aqui estão as suas ultimas ações</h2>
+            </div>
+
+            <div className={styles.container}>
+                <Acoes />
+            </div>
+
         </div>
     )
 }
